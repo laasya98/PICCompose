3314297c4c4099b02468d0416e218526cfa8a419
Laasya Renganathan <lpr46@cornell.edu>
2019-12-16 18:27:33 -0500

    more web updates

--- lpr46_tev22_dms486/js/main.js
+++ lpr46_tev22_dms486/js/main.js
@@ -51,7 +51,7 @@
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				$('html, body').animate({
-					scrollTop: (target.offset().top - navHeight)
+					scrollTop: (target.offset().top - navHeight + 2)
 				}, 1000, "easeInOutExpo");
 				return false;
 			}
